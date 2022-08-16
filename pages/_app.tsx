af553488c39c3f254f96bda8f3021f948fab085f
Federico Gonzalez <alambratore@hotmail.com>
2022-08-16 08:29:12 -0300

    feat: :tada:

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,21 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
+import {CssBaseline, ThemeProvider} from '@mui/material'
+import {Provider} from 'react-redux'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import {darkTheme, lightTheme} from '../themes'
+
+import '../styles/globals.css'
+import {store} from '../redux'
+
+const MyApp = ({Component, pageProps}: AppProps) => {
+  return (
+    <Provider store={store}>
+      <ThemeProvider theme={darkTheme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </Provider>
+  )
 }
 
 export default MyApp
